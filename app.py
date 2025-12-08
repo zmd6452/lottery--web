d387a79e4292bcb2a758c4fd9497667134807f61
Zul <rukia6452@gmail.com>
2025-12-09 04:08:33 +0800

    Simplify app.run by removing environment variable for port

--- app.py
+++ app.py
@@ -51,7 +51,4 @@ def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 
 
-import os
-port = int(os.environ.get("PORT", 5000))
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+app.run(host='0.0.0.0', port=5000)
