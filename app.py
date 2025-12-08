550f2f8841921faaeea53a4acf04a4c5d6bdf8b5
Zul <rukia6452@gmail.com>
2025-12-09 02:41:03 +0800

    Use environment variable for port configuration

--- app.py
+++ app.py
@@ -50,5 +50,8 @@ def analyze():
 def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 
+
+import os
+port = int(os.environ.get("PORT", 5000))
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=port)
